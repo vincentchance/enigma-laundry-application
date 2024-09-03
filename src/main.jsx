013e1b5511d8f4fc770a267f932dc8836a236958
vincent chance <vincentchance71@gmail.com>
2024-09-03 23:39:04 +0700

    [add reducers : redux] create store and reducers

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,23 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { NextUIProvider } from '@nextui-org/react';
-
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import { BrowserRouter } from 'react-router-dom';
+import { reducers } from './store/store.js';
 import App from './App.jsx';
 import './index.css';
-//import { store } from './store/store.js';
+
+const store = createStore(reducers)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-		<NextUIProvider>
-			<BrowserRouter>
-				<App />
-			</BrowserRouter>
-		</NextUIProvider>
+		<Provider store={store} >
+			<NextUIProvider>
+				<BrowserRouter>
+					<App />
+				</BrowserRouter>
+			</NextUIProvider>
+		</Provider>
   </StrictMode>,
 )
---
+++ src/store/authReducer.js
@@ -0,0 +1,16 @@
+const DEFAULT_STATE = {
+	authData: JSON.parse(localStorage.getItem("authData")) || null,
+}
+
+export const authReducer = (state = DEFAULT_STATE, action) => {
+	switch (action.type) {
+		case "LOGIN" :
+			localStorage.setItem("authData", JSON.stringify(action.payload.authData))
+			return {...state, authData: action.payload.authData};
+		case "LOGOUT" : 
+			localStorage.removeItem("authData");
+			return {...state, authData: null};
+		default:
+			return state;
+	}
+}
