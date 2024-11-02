1011bfede7cdad3bc836656538170dfc5499165a
vincent chance <vincentchance71@gmail.com>
2024-11-02 16:25:23 +0700

    menambah peforma web enigma laundry

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,27 @@
+import React, { Suspense, lazy } from 'react';
 import LoginPage from './auth/LoginPage';
-import Dashboard from './pages/Dashboard.jsx';
-import Customers from './pages/Customers.jsx';
-import Products from './pages/Products.jsx';
 import RegisterPage from './auth/RegisterPage';
-import { Routes, Route } from "react-router-dom";
+import { Route, Routes } from 'react-router-dom';
 import { Toaster } from 'sonner';
+
+const Dashboard = lazy(() => import('./pages/Dashboard.jsx'));
+const Customers = lazy(() => import('./pages/Customers'));
+const Products = lazy(() => import('./pages/Products'));
 
 function App() {
 
   return (
 	<>
 		<Toaster position="top-center" />
-		<Routes>
-			<Route element={<LoginPage />} path="/" />
-			<Route element={<RegisterPage />} path="/register" />
-			<Route element={<Dashboard />} path="/dashboard/:username" />
-			<Route element={<Customers />} path="/customer/:username" />
-			<Route element={<Products />} path="/product/:username" />
-		</Routes>
+		<Suspense>
+			<Routes>
+				<Route element={<LoginPage />} path="/" />
+				<Route element={<RegisterPage />} path="/register" />
+				<Route element={<Dashboard />} path="/dashboard/:username" />
+				<Route element={<Customers />} path="/customer/:username" />
+				<Route element={<Products />} path="/product/:username" />
+			</Routes>
+		</Suspense>
 	</>
   )
 }
